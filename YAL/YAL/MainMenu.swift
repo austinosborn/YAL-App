a2fdf46a3e0e1e7e8bb2298b4fc332a0ee03f35d
austinosborn <austin.osborn1@gmail.com>
2018-11-13 19:39:22 -0600

    Created UIColor convenience init with hexString and alpha. Overhauled aesthetics (Updated launch screen, navigationItem view, button coloring). Added banner that links to organization website

--- YAL/YAL/MainMenu.swift
+++ YAL/YAL/MainMenu.swift
@@ -15,13 +15,27 @@ class MainMenu: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Navigation controller/status bar styling
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0784, green: 0.21568, blue: 0.37254, alpha: 1)
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
         self.navigationController?.navigationBar.tintColor = UIColor.white
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
-        self.navigationItem.title = "Main Menu"
-        UIApplication.shared.statusBarStyle = .lightContent
-        self.view.backgroundColor = UIColor(red: 0.0784, green: 0.21568, blue: 0.37254, alpha: 1)
+        let title = UIImageView(image: UIImage(named: "eagle")?.withRenderingMode(.alwaysTemplate))
+        title.tintColor = UIColor(hexString: "#14375F", alpha: 1) ?? UIColor.black
+        title.contentMode = .scaleAspectFill
+        self.navigationItem.titleView = title
+        UIApplication.shared.statusBarStyle = .default
+        self.view.backgroundColor = UIColor(hexString: "#14375F", alpha: 1)
         
+        
+        //Header Icon
+        let header = UIImageView(image: UIImage(named: "yal_icon"))
+        header.contentMode = .scaleAspectFit
+        self.view.addSubview(header)
+        header.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(20)
+            make.height.equalToSuperview().multipliedBy(0.2)
+            make.width.equalToSuperview().multipliedBy(0.4)
+        }
         //Stackview holds all menu buttons
         let stack = UIStackView(arrangedSubviews: [
             createButton(name: "Officer Directory", selector: #selector(goToOfficers)),
@@ -43,7 +57,29 @@ class MainMenu: UIViewController {
         self.view.addSubview(stack)
         stack.snp.makeConstraints { (make) in
             make.centerX.centerY.equalToSuperview()
-            make.width.height.equalToSuperview().multipliedBy(0.6)
+            make.width.equalToSuperview().multipliedBy(0.8)
+            make.height.equalToSuperview().multipliedBy(0.6)
+        }
+        
+        let banner = UIView()
+        //Setup subview of banners first
+        let bannerImage = UIImageView(image: UIImage(named: "banner"))
+        banner.contentMode = .scaleAspectFill
+        banner.addSubview(bannerImage)
+        bannerImage.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        let bannerButton = UIButton()
+        bannerButton.addTarget(self, action: #selector(goToWebsite), for: .touchUpInside)
+        banner.addSubview(bannerButton)
+        bannerButton.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        //Now add banner onto screen
+        self.view.addSubview(banner)
+        banner.snp.makeConstraints { (make) in
+            make.bottom.right.left.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.125)
         }
         
         //Banner 
@@ -75,13 +111,17 @@ class MainMenu: UIViewController {
         self.present(alert, animated: true)
     }
     
+    @objc func goToWebsite() {
+        UIApplication.shared.open(URL(string: "https://www.yaliberty.org")!, options: [:], completionHandler: nil)
+    }
+    
     func createButton(name: String, selector: Selector) -> UIView {
         let item = UIView()
-        item.backgroundColor = UIColor.white
+        item.backgroundColor = UIColor(hexString: "FDC50E", alpha: 1)
         
         let label = UILabel()
         label.text = name
-        label.textColor =  UIColor(red: 0.0784, green: 0.21568, blue: 0.37254, alpha: 1)
+        label.textColor =  UIColor(hexString: "#14375F", alpha: 1)
         label.contentMode = .center
         label.textAlignment = .center
         label.adjustsFontSizeToFitWidth = true
@@ -101,6 +141,37 @@ class MainMenu: UIViewController {
     }
 
 
+}
+
+extension UIColor {
+    convenience init?(hexString: String, alpha: Float) {
+        var localString = hexString
+        localString.removeAll { (char) -> Bool in
+            let validChars = "0123456789ABCDEFabcdef"
+            if (validChars.contains(char)) {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+        if localString.count != 6 {
+            return nil
+        }
+        
+        guard let red = UInt8(localString.prefix(2), radix: 16) else { return nil }
+        let greenIndexStart = localString.index(localString.startIndex, offsetBy: 2)
+        let greenIndexEnd = localString.index(localString.endIndex, offsetBy: -2)
+        guard let green = UInt8(localString.substring(with: greenIndexStart..<greenIndexEnd), radix: 16) else { return nil }
+        guard let blue = UInt8(localString.suffix(2), radix: 16) else { return nil }
+        
+        let redFloat : Float = (Float(red) / 255)
+        let blueFloat : Float = (Float(blue) / 255)
+        let greenFloat : Float = (Float(green) / 255)
+        
+        self.init(red: CGFloat(redFloat), green: CGFloat(greenFloat), blue: CGFloat(blueFloat), alpha: CGFloat(alpha))
+
+    }
 }
 //
 
